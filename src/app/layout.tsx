bcf9b343839e350bc5b0378690fa7e62c1848f84
comerc <andrew.kachanov@gmail.com>
2025-08-21 14:49:00 +0300

    Fix overflow-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-hidden">
       <body className="flex h-screen flex-col">
         <FathomAnalytics />
         <SettingsContextProvider>
